2feb1a8befce07291c2c28adbaaac3ed46843ebb
mcarr67 <mcarr67@gmail.com>
2012-12-07 22:55:03 -0800

    Update manifests/init.pp
    create variable java_base_dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,25 +4,25 @@ class oracle-java {
     default => 'i586',
   }
   $package_name = "${java_distrib}-${java_version}-linux-${arch}.tar.gz"
-  file { '/usr/local/java':
+  file { "${java_base_dir}/java':
     ensure => directory,
   }
-  file { "/usr/local/java/${package_name}":
+  file { "${java_base_dir}/java/${package_name}":
     ensure  => present,
     source  => "puppet:///modules/oracle-java/${package_name}",
-    require => File['/usr/local/java'],
+    require => File["${java_base_dir}/java"],
     notify  => Exec['unpack-java'],
   }
   exec { 'unpack-java':
     command     => "tar zxf ${package_name}",
-    cwd         => '/usr/local/java/',
+    cwd         => "${java_base_dir}/java/",
     path        => '/bin',
     refreshonly => true,
     notify      => Exec['rename-java-dir'],
   }
   exec { 'rename-java-dir':
     command     => "mv jre1.* ${java_version}",
-    cwd         => '/usr/local/java/',
+    cwd         => "${java_base_dir}/java/",
     path        => '/bin',
     refreshonly => true,
   }
